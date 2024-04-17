783d62ad74e9355f97c4ac6eba26530a66795b32
liurubin <liu.rubin@eflagcomm.com>
2024-04-17 14:10:00 +0800

    更新

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,9 +1,9 @@
 import request from 'umi-request';
 
-export function picture(baseURL: string, path: string, rawBody: any) {
+export function picture(baseURL: any, path: string, rawBody: any) {
   return request.post(`/${path}/get`, { data: rawBody, prefix: baseURL || '/' });
 }
 
-export function check(baseURL: string, path: string, rawBody: any) {
+export function check(baseURL: any, path: string, rawBody: any) {
   return request.post(`/${path}/check`, { data: rawBody, prefix: baseURL || '/' });
 }
